969bbffda1283c249c488a4022a6a7ae9b31d040
Shakshi sharma <sakshi.sharma@my.jcu.edu.au>
2025-06-10 19:41:25 +1000

    "Add randoms.py with randint, randrange, uniform examples and explanations"

---
+++ Practical3/randoms.py
@@ -0,0 +1,13 @@
+import random
+
+# Line 1
+print(random.randint(5, 20))  # Smallest: 5, Largest: 20
+
+# Line 2
+print(random.randrange(3, 10, 2))  # Smallest: 3, Largest: 9. Could NOT be 4.
+
+# Line 3
+print(random.uniform(2.5, 5.5))  # Smallest: 2.5, Largest: 5.5
+
+# Produce a random number between 1 and 100 inclusive
+print(random.randint(1, 100))
