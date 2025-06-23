6c2da8825f81bf957aa64e77a880f5fee21329d5
Shakshi sharma <sakshi.sharma@my.jcu.edu.au>
2025-06-23 23:07:23 +1000

    "Implement word_occurrences.py with word counting and aligned display"

---
+++ Practical5/word_occurrences.py
@@ -0,0 +1,11 @@
+text = input("Text: ")
+word_to_count = {}
+words = text.split()
+
+for word in words:
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+
+max_length = max(len(word) for word in word_to_count)
+
+for word in sorted(word_to_count):
+    print(f"{word:{max_length}} : {word_to_count[word]}")
